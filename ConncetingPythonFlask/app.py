e0b060977b812e7b0deb713281d2d5bd3b3b8f7e
unknown <vinodkumar9676150@gmail.com>
2023-04-27 12:01:01 +0530

    update files

---
+++ ConncetingPythonFlask/app.py
@@ -0,0 +1,12 @@
+print('Welcome to Python 101 on Scrimba')
+name="Vinod"
+print(f'Hi {name} you\'re welcome')
+age=input("Enter you're age")
+print(f'Hi {name} you\'re age is {age}')
+# Variable concept
+falied_subject='6'
+name='john'
+print(name + ' is falied in '+falied_subject + 'subjects.')
+print(name + ' is attemeting ' + falied_subject + 'but he fails')
+name='Roy'
+print(name + ' is good at the mathematics')
