da7756489923ad11fd07fcb60cd347666f450b1c
unknown <vinodkumar9676150@gmail.com>
2023-04-28 21:49:39 +0530

    update files

--- ConncetingPythonFlask/if_else.py
+++ ConncetingPythonFlask/if_else.py
@@ -8,3 +8,21 @@ elif not(is_rain) and is_cold:
     print("Bring jacket")
 else:
     print("don't take umbrella")
+
+mode = input('Enter math operation(+,-,*,/) or f for Celsius to Fahrenheit conversion: ')
+num1 = float(input('Enter first number: '))
+if mode.lower() == 'f':
+    print(f'{num1} Celsius is equivalent to {(num1*9/5)+32 } fahrenheit')
+else:
+    num2 = float(input('Enter second number: '))
+
+    if mode == '+':
+        print(f'Answer is: {num1 + num2}')
+    elif mode == '-':
+        print(f'Answer is: {num1 - num2}')
+    elif mode == '*':
+        print(f'Answer is: {num1 * num2}')
+    elif mode == '/':
+        print(f'Answer is: {num1 / num2}')
+    else:
+        print('Input error!') 
