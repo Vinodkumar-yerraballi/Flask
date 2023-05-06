1a5401e5070ef3108be09ea0558ef34f0c4905b1
unknown <vinodkumar9676150@gmail.com>
2023-05-06 15:23:00 +0530

    add files

--- ConncetingPythonFlask/if_else.py
+++ ConncetingPythonFlask/if_else.py
@@ -62,3 +62,40 @@ for i in range(1,len(list_a)):
 usernames_length=input()
 length=len(usernames_length)
 print(length)
+# word = input()
+# length = len(word)
+# index = word[-1]
+# print(index)
+
+print(input()[-1])
+print(len(input())-1)
+#Remove the last two element in the given string
+print(len(input())-2)
+# The output should be Q****
+word=input()
+print(word[0]+"*"*(len(word)-1))
+#divided the length of the string divided by 2
+print(len(input())/2)
+a = [print("* *") for row in range(3)]
+for i in range(3):
+    print("* *")
+for i in range(3):
+    print("* * *")
+for i in range(1,4):
+    print("* "*i)
+w = input()
+print("*"*len(w)+ " "+w +" "+ "*"*len(w))
+w2 = input()
+print(len(w2)-1)
+w1 = input()
+w2 = input()
+print(w2)
+print("###")
+print(w1)
+w =input()
+print(w[0])
+print(w[-1])
+a= input()
+b = input()
+c = input()
+print(a[:1]+b[:1]+c[:1])
